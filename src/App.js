d5419b45b76c6d68c3b553585c6ab1896bc086a5
jaboulos <jab12288@gmail.com>
2021-02-27 16:13:20 -0800

    Refactor to class component, fetch api data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
-// named exports
+// named imports
 import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './api';
+
 import styles from './App.module.css';
 
 class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+    // console.log(data);
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
