4d280ed08785639512a6d953bb53880767166ca0
jaboulos <jab12288@gmail.com>
2021-02-27 16:14:04 -0800

    Initial setup and styling for Card component

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 
-const Cards = () => {
-  return <h1>Cards</h1>;
+import styles from './Cards.module.css';
+
+// gutterBottom style is for padding at the bottom
+const Cards = (props) => {
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify='center'>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color='textSecondary' gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant='h5'>Real Data</Typography>
+            <Typography color='textSecondary'>Real Date</Typography>
+            <Typography variant='body2'>
+              Number of active Covid-19 cases
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
