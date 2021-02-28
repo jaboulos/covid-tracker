b58b20fb254935fce843e66a84acf4d65b10a963
jaboulos <jab12288@gmail.com>
2021-02-28 14:44:43 -0800

    Add additional APIs and refactors

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
 
     // const modifiedData = {
     //   confirmed: data.confirmed,
@@ -31,7 +37,7 @@ export const fetchData = async () => {
       lastUpdate,
     };
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
 };
 
@@ -49,4 +55,17 @@ export const fetchDailyData = async () => {
 
     return modifiedData;
   } catch (error) {}
+};
+
+export const fetchCountries = async () => {
+  try {
+    // const response = await axios.get(`${url}/countries`);
+    // destructure
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name);
+  } catch (error) {
+    console.log(error);
+  }
 };
